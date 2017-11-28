0bb126908cd1eee897ccac1bdfd752615e877821
albertpark <albert_park@hotmail.com>
2017-11-28 18:33:08 -0500

    Adding define.php for global variables

---
+++ define.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+
+define('__ROOT__', dirname(dirname(__FILE__)));
+define('__DOCUMENT__', dirname(__FILE__));
+define('__SERVER__', dirname(__FILE__));
+
+?>

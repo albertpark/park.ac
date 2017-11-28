17e6f1067f045083e43f4808aa074233f4af9b4c
albertpark <albert_park@hotmail.com>
2017-11-28 18:34:26 -0500

    Using globals and library functions

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?
 
-echo 'park.ac';
+require('define.php');
+include_once(__ROOT__ . DS . 'library' . DS . 'helper.php');
+
+// client-side controlled value
+echo $_SERVER[HTTP_HOST];
+// server-side controlled value
+//echo $_SERVER[SERVER_NAME];
+
+#debug($_SERVER);
 
 ?>
